6245cc7e21018a3d3f50659166ba16eb976bf06a
Philip Wang <Zijian.Wang@Tufts.edu>
2018-10-31 20:39:56 -0400

    v0

--- HSHSmobile/src/Signup.js
+++ HSHSmobile/src/Signup.js
@@ -32,92 +32,95 @@ class Signup extends Component {
     render() {
         return (
             <ScrollView style={{flex: 1, flexDirection: 'column'}}>
-                <FormLabel>Signup Key (Contact the admin for this)</FormLabel>
-                <FormInput onChangeText={(text) => {
-                    this.setState({k: text})
-                }}/>
-                <FormValidationMessage>{this.state.keyErr}</FormValidationMessage>
-                <FormLabel>Email Address</FormLabel>
-                <FormInput onChangeText={(text) => {
-                    this.setState({email: text})
-                }}/>
-                <FormValidationMessage>{this.state.emailErr}</FormValidationMessage>
-                <FormLabel>Password</FormLabel>
-                <FormInput onChangeText={(text) => {
-                    const lengthCheck = text.length >= 8;
-                    const upperCaseCheck = /[A-Z]/.test(text);
-                    const lowerCaseCheck = /[a-z]/.test(text);
-                    const numCheck = /\d/.test(text);
-                    this.setState({password: text, lengthCheck, upperCaseCheck, lowerCaseCheck, numCheck});
-
-                }}/>
-                <FormValidationMessage><Text style={{color: "black"}}>Your password must contain the
-                    following:</Text></FormValidationMessage>
-                <FormValidationMessage>
-                    {this.state.lengthCheck ?
-                        <Icon name="md-checkmark" size={20} color="green"/> :
-                        <Icon name="md-close" size={20} color="red"/>
-                    }
-                    <Text style={{...(this.state.lengthCheck ? {color: 'green'} : {color: 'red'})}}>
-                        {' '}At least 8 characters
-                    </Text>
-                </FormValidationMessage>
-                <FormValidationMessage>
-                    {this.state.upperCaseCheck ?
-                        <Icon name="md-checkmark" size={20} color="green"/> :
-                        <Icon name="md-close" size={20} color="red"/>
-                    }
-                    <Text style={{...(this.state.upperCaseCheck ? {color: 'green'} : {color: 'red'})}}>
-                        {' '}At least one uppercase letter
-                    </Text>
-                </FormValidationMessage>
-                <FormValidationMessage>
-                    {this.state.lowerCaseCheck ?
-                        <Icon name="md-checkmark" size={20} color="green"/> :
-                        <Icon name="md-close" size={20} color="red"/>
-                    }
-                    <Text style={{...(this.state.lowerCaseCheck ? {color: 'green'} : {color: 'red'})}}>
-                        {' '}At least one lowercase letter
-                    </Text>
-                </FormValidationMessage>
-                <FormValidationMessage>
-                    {this.state.numCheck ?
-                        <Icon name="md-checkmark" size={20} color="green"/> :
-                        <Icon name="md-close" size={20} color="red"/>
-                    }
-                    <Text style={{...(this.state.numCheck ? {color: 'green'} : {color: 'red'})}}>
-                        {' '}At least one number
-                    </Text>
-                </FormValidationMessage>
-                <Button onPress={() => {
-                    if (this.state.k === "") {
-                        this.setState({keyErr: errMsg});
-                        return;
-                    } else {
-                        this.setState({keyErr: ""});
-                    }
-                    if (this.state.email === "") {
-                        this.setState({emailErr: errMsg});
-                        return;
-                    } else {
-                        this.setState({emailErr: ""});
-                    }
-                    if (!(this.state.lengthCheck && this.state.upperCaseCheck && this.state.lowerCaseCheck && this.state.numCheck)) {
-                        return;
-                    }
-                    fetch('https://us-central1-hshs-street-team.cloudfunctions.net/signUpPOST', {
-                        body: JSON.stringify({
-                            email: this.state.email,
-                            password: this.state.password,
-                            signupKey: this.state.k
-                        }), // must match 'Content-Type' header
-                        method: 'POST',
-                        headers: {'content-type': 'application/json'}
-                    }).then(response => console.log(response))
-                }} title='Submit'/>
+                <Text>Sign up through the app is currently unavailable. Please contact street team admin if you need a volunteer account</Text>
             </ScrollView>
         )
     }
 };
+
+
+{/* <FormLabel>Signup Key (Contact the admin for this)</FormLabel>
+<FormInput onChangeText={(text) => {
+    this.setState({k: text})
+}}/>
+<FormValidationMessage>{this.state.keyErr}</FormValidationMessage>
+<FormLabel>Email Address</FormLabel>
+<FormInput onChangeText={(text) => {
+    this.setState({email: text})
+}}/>
+<FormValidationMessage>{this.state.emailErr}</FormValidationMessage>
+<FormLabel>Password</FormLabel>
+<FormInput onChangeText={(text) => {
+    const lengthCheck = text.length >= 8;
+    const upperCaseCheck = /[A-Z]/.test(text);
+    const lowerCaseCheck = /[a-z]/.test(text);
+    const numCheck = /\d/.test(text);
+    this.setState({password: text, lengthCheck, upperCaseCheck, lowerCaseCheck, numCheck});
+
+}}/>
+<FormValidationMessage><Text style={{color: "black"}}>Your password must contain the
+    following:</Text></FormValidationMessage>
+<FormValidationMessage>
+    {this.state.lengthCheck ?
+        <Icon name="md-checkmark" size={20} color="green"/> :
+        <Icon name="md-close" size={20} color="red"/>
+    }
+    <Text style={{...(this.state.lengthCheck ? {color: 'green'} : {color: 'red'})}}>
+        {' '}At least 8 characters
+    </Text>
+</FormValidationMessage>
+<FormValidationMessage>
+    {this.state.upperCaseCheck ?
+        <Icon name="md-checkmark" size={20} color="green"/> :
+        <Icon name="md-close" size={20} color="red"/>
+    }
+    <Text style={{...(this.state.upperCaseCheck ? {color: 'green'} : {color: 'red'})}}>
+        {' '}At least one uppercase letter
+    </Text>
+</FormValidationMessage>
+<FormValidationMessage>
+    {this.state.lowerCaseCheck ?
+        <Icon name="md-checkmark" size={20} color="green"/> :
+        <Icon name="md-close" size={20} color="red"/>
+    }
+    <Text style={{...(this.state.lowerCaseCheck ? {color: 'green'} : {color: 'red'})}}>
+        {' '}At least one lowercase letter
+    </Text>
+</FormValidationMessage>
+<FormValidationMessage>
+    {this.state.numCheck ?
+        <Icon name="md-checkmark" size={20} color="green"/> :
+        <Icon name="md-close" size={20} color="red"/>
+    }
+    <Text style={{...(this.state.numCheck ? {color: 'green'} : {color: 'red'})}}>
+        {' '}At least one number
+    </Text>
+</FormValidationMessage>
+<Button onPress={() => {
+    if (this.state.k === "") {
+        this.setState({keyErr: errMsg});
+        return;
+    } else {
+        this.setState({keyErr: ""});
+    }
+    if (this.state.email === "") {
+        this.setState({emailErr: errMsg});
+        return;
+    } else {
+        this.setState({emailErr: ""});
+    }
+    if (!(this.state.lengthCheck && this.state.upperCaseCheck && this.state.lowerCaseCheck && this.state.numCheck)) {
+        return;
+    }
+    fetch('https://us-central1-hshs-street-team.cloudfunctions.net/signUpPOST', {
+        body: JSON.stringify({
+            email: this.state.email,
+            password: this.state.password,
+            signupKey: this.state.k
+        }), // must match 'Content-Type' header
+        method: 'POST',
+        headers: {'content-type': 'application/json'}
+    }).then(response => console.log(response))
+}} title='Submit'/> */}
 
 export default Signup;
