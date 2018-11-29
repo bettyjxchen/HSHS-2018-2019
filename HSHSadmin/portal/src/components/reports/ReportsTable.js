c003d40211a11c50d841773fe27e3d8952fc69f4
Betty Chen <bettyjxchen@gmail.com>
2018-11-28 22:10:17 -0500

    commit: disabled delete button, map names

--- HSHSadmin/portal/src/components/reports/ReportsTable.js
+++ HSHSadmin/portal/src/components/reports/ReportsTable.js
@@ -15,10 +15,8 @@ import Paper from "@material-ui/core/Paper";
 import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
-import DeleteIcon from "@material-ui/icons/Delete";
 import { lighten } from "@material-ui/core/styles/colorManipulator";
-import * as firebase from 'firebase';
-
+import * as firebase from "firebase";
 
 //create data
 let counter = 0;
@@ -56,27 +54,23 @@ const rows = [
 function desc(a, b, orderBy) {
 	//TODO @Phil this is the function I changed but I don't see it getting called anywhere
 	let date1 = a.id.split("-");
-	let date2 = b.id.split("-");;
+	let date2 = b.id.split("-");
 
 	if (date1[2] < date2[2]) {
 		return -1;
-	} 
-	else if (date1[2] > date2[2]) {
+	} else if (date1[2] > date2[2]) {
 		return 1;
-	} 
-	else {
+	} else {
 		if (date1[1] < date2[1]) {
 			return -1;
-		}
-		else if (date1[1] > date2[1]) {
+		} else if (date1[1] > date2[1]) {
 			return 1;
-		} 
-		else {
-			return (date1[0] > date2[0]) ? -1 : 1;
+		} else {
+			return date1[0] > date2[0] ? -1 : 1;
 		}
 	}
-	
-	console.error('Date sorting funciton error');
+
+	console.error("Date sorting funciton error");
 }
 
 function stableSort(array, cmp) {
@@ -96,30 +90,17 @@ function getSorting(order, orderBy) {
 }
 
 class EnhancedTableHead extends React.Component {
-
 	createSortHandler = property => event => {
 		this.props.onRequestSort(event, property);
 	};
 
 	render() {
-		const {
-			onSelectAllClick,
-			order,
-			orderBy,
-			numSelected,
-			rowCount
-		} = this.props;
+		const { order, orderBy, rowCount } = this.props;
 
 		return (
 			<TableHead>
 				<TableRow>
-					<TableCell padding="checkbox">
-						<Checkbox
-							indeterminate={numSelected > 0 && numSelected < rowCount}
-							checked={numSelected === rowCount}
-							onChange={onSelectAllClick}
-						/>
-					</TableCell>
+					<TableCell padding="checkbox" />
 					{rows.map(row => {
 						return (
 							<TableCell
@@ -151,9 +132,7 @@ class EnhancedTableHead extends React.Component {
 }
 
 EnhancedTableHead.propTypes = {
-	numSelected: PropTypes.number.isRequired,
 	onRequestSort: PropTypes.func.isRequired,
-	onSelectAllClick: PropTypes.func.isRequired,
 	order: PropTypes.string.isRequired,
 	orderBy: PropTypes.string.isRequired,
 	rowCount: PropTypes.number.isRequired
@@ -166,13 +145,13 @@ const toolbarStyles = theme => ({
 	highlight:
 		theme.palette.type === "light"
 			? {
-				color: theme.palette.secondary.main,
-				backgroundColor: lighten(theme.palette.secondary.light, 0.85)
-			}
+					color: theme.palette.secondary.main,
+					backgroundColor: lighten(theme.palette.secondary.light, 0.85)
+			  }
 			: {
-				color: theme.palette.text.primary,
-				backgroundColor: theme.palette.secondary.dark
-			},
+					color: theme.palette.text.primary,
+					backgroundColor: theme.palette.secondary.dark
+			  },
 	spacer: {
 		flex: "1 1 100%"
 	},
@@ -185,46 +164,31 @@ const toolbarStyles = theme => ({
 });
 
 let EnhancedTableToolbar = props => {
-	const { numSelected, classes } = props;
+	const { classes } = props;
 
 	return (
-		<Toolbar
-			className={classNames(classes.root, {
-				[classes.highlight]: numSelected > 0
-			})}
-		>
+		<Toolbar className={classNames(classes.root)}>
 			<div className={classes.title}>
-				{numSelected > 0 ? (
-					<Typography color="inherit" variant="subtitle1">
-						{numSelected} selected
+				{
+					<Typography variant="h6" id="tableTitle">
+						Reports
 					</Typography>
-				) : (
-						<Typography variant="h6" id="tableTitle">
-							Reports
-					</Typography>
-					)}
+				}
 			</div>
 			<div className={classes.spacer} />
 			<div className={classes.actions}>
-				{numSelected > 0 ? (
-					<Tooltip title="Delete">
-						<IconButton aria-label="Delete">
-							<DeleteIcon />
-						</IconButton>
+				{
+					<Tooltip title="Filter list">
+						<IconButton aria-label="Filter list" />
 					</Tooltip>
-				) : (
-						<Tooltip title="Filter list">
-							<IconButton aria-label="Filter list" />
-						</Tooltip>
-					)}
+				}
 			</div>
 		</Toolbar>
 	);
 };
 
 EnhancedTableToolbar.propTypes = {
-	classes: PropTypes.object.isRequired,
-	numSelected: PropTypes.number.isRequired
+	classes: PropTypes.object.isRequired
 };
 
 EnhancedTableToolbar = withStyles(toolbarStyles)(EnhancedTableToolbar);
@@ -245,25 +209,34 @@ const styles = theme => ({
 class ReportsTable extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = { data: [], order: 'asc', orderBy: 'date', selected: [], page: 0, rowsPerPage: 5 };
+		this.state = {
+			data: [],
+			order: "asc",
+			orderBy: "date",
+			page: 0,
+			rowsPerPage: 5
+		};
 	}
 
 	componentDidMount() {
 		let self = this;
-		firebase.database().ref("/dateEntry/").once('value').then(function (snapshot) {
-			var val = snapshot.val();
-			var reports_list = [];
-			Object.keys(val).forEach((key) => {
-				var value = val[key];
-				if (!value.Supply)
-					value.Supply = {};
-				var value = { id: key, value: val[key] };
-				reports_list.push(value);
+		firebase
+			.database()
+			.ref("/dateEntry/")
+			.once("value")
+			.then(function(snapshot) {
+				var val = snapshot.val();
+				var reports_list = [];
+				Object.keys(val).forEach(key => {
+					var value = val[key];
+					if (!value.Supply) value.Supply = {};
+					var value = { id: key, value: val[key] };
+					reports_list.push(value);
+				});
+				self.setState({
+					data: reports_list
+				});
 			});
-			self.setState({
-				data: reports_list
-			});
-		});
 	}
 
 	handleRequestSort = (event, property) => {
@@ -277,35 +250,6 @@ class ReportsTable extends React.Component {
 		this.setState({ order, orderBy });
 	};
 
-	handleSelectAllClick = event => {
-		if (event.target.checked) {
-			this.setState(state => ({ selected: state.data.map(n => n.id) }));
-			return;
-		}
-		this.setState({ selected: [] });
-	};
-
-	handleClick = (event, id) => {
-		const { selected } = this.state;
-		const selectedIndex = selected.indexOf(id);
-		let newSelected = [];
-
-		if (selectedIndex === -1) {
-			newSelected = newSelected.concat(selected, id);
-		} else if (selectedIndex === 0) {
-			newSelected = newSelected.concat(selected.slice(1));
-		} else if (selectedIndex === selected.length - 1) {
-			newSelected = newSelected.concat(selected.slice(0, -1));
-		} else if (selectedIndex > 0) {
-			newSelected = newSelected.concat(
-				selected.slice(0, selectedIndex),
-				selected.slice(selectedIndex + 1)
-			);
-		}
-
-		this.setState({ selected: newSelected });
-	};
-
 	handleChangePage = (event, page) => {
 		this.setState({ page });
 	};
@@ -314,24 +258,20 @@ class ReportsTable extends React.Component {
 		this.setState({ rowsPerPage: event.target.value });
 	};
 
-	isSelected = id => this.state.selected.indexOf(id) !== -1;
-
 	render() {
 		const { classes } = this.props;
-		const { data, order, orderBy, selected, rowsPerPage, page } = this.state;
+		const { data, order, orderBy, rowsPerPage, page } = this.state;
 		const emptyRows =
 			rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
 
 		return (
 			<Paper className={classes.root}>
-				<EnhancedTableToolbar numSelected={selected.length} />
+				<EnhancedTableToolbar />
 				<div className={classes.tableWrapper}>
 					<Table className={classes.table} aria-labelledby="tableTitle">
 						<EnhancedTableHead
-							numSelected={selected.length}
 							order={order}
 							orderBy={orderBy}
-							onSelectAllClick={this.handleSelectAllClick}
 							onRequestSort={this.handleRequestSort}
 							rowCount={data.length}
 						/>
@@ -339,34 +279,46 @@ class ReportsTable extends React.Component {
 							{stableSort(data, getSorting(order, orderBy))
 								.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
 								.map(n => {
-									const isSelected = this.isSelected(n.id);
+									//map data to form fields
+									n.value.Headcount = {
+										"E-beds called in": n.value.Headcount["bedsCalled"],
+										"People spoken to about resources":
+											n.value.Headcount["peopleAboutResource"],
+										"Total people spoken to": n.value.Headcount["totalPeople"],
+										"Youth spoken to": n.value.Headcount["youth"]
+									};
+
 									return (
-										<TableRow
-											hover
-											onClick={event => this.handleClick(event, n.id)}
-											role="checkbox"
-											aria-checked={isSelected}
-											tabIndex={-1}
-											key={n.id}
-											selected={isSelected}
-										>
-											<TableCell padding="checkbox">
-												<Checkbox checked={isSelected} />
-											</TableCell>
+										<TableRow hover role="checkbox" tabIndex={-1} key={n.id}>
+											<TableCell style={{ paddingLeft: "15px" }} />
 											<TableCell component="th" scope="row" padding="default">
 												{n.id}
 											</TableCell>
 											<TableCell padding="default">
 												<ul style={{ paddingLeft: "15px" }}>
 													{Object.keys(n.value.Supply).map(item => {
-														return <li key={n.id + item}>{item}: {n.value.Supply[item]}</li>
+														return (
+															<li
+																style={{ paddingTop: "6px" }}
+																key={n.id + item}
+															>
+																{item}: {n.value.Supply[item]}
+															</li>
+														);
 													})}
 												</ul>
 											</TableCell>
 											<TableCell style={{ paddingLeft: "45px" }}>
 												<ul style={{ paddingLeft: "15px" }}>
 													{Object.keys(n.value.Headcount).map(item => {
-														return <li key={n.id + item}>{item}: {n.value.Headcount[item]}</li>
+														return (
+															<li
+																style={{ paddingTop: "6px" }}
+																key={n.id + item}
+															>
+																{item}: {n.value.Headcount[item]}
+															</li>
+														);
 													})}
 												</ul>
 											</TableCell>
